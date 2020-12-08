e0102fc6bab5d848f4a634cac1812ddb1e2527fc
Nick van Hurck <nickvanhurck@hotmail.be>
2020-12-08 14:42:17 +0100

    documentation (heuristics file)

--- src/scripts/heuristics.js
+++ src/scripts/heuristics.js
@@ -1,21 +1,3 @@
-//TODO: maybe: if not feedback is given generate default feedback
-//TODO: document the code
-
-/* EXPLANATION
-* Ik wil een makkelijkere uitvoering methode maken voor een bepaalde oefening:
-*
-* We willen dus een oefening encoderen door bepaalde checks
-* Exercise heeft meerdere checks die hij moet doen
-* Een check kan verschillende dingen zijn
-* - Check of 2 gegeven punten horizontaal of verticaal zijn (dit kan geimplementeerd worden door  puntje 2 hoek 0
-* - Check of een lijn tussen 2 punten in een bepaalde hoek ligt (dit kan horizontaal of verticaal)
-* - Check of een lijn tussen 3 punten (de hoek die hieruit volgt) in een bepaalde hoek ligt
-* - Check afstand tussen 2 punten
-* - check afstand tussen 2 lijnen (middelpunten van elke lijn)
-* */
-
-import {getAdjacentKeyPoints} from "@tensorflow-models/posenet";
-
 /**
  * An Enum for the keypoints of the body
  */
@@ -40,14 +22,17 @@ export const Keypoints = {
 };
 
 //===============================================HELPERS==============================================================
+// calculated distance between 2 points
 function distanceBetween2Points(point1, point2) { //length of line
   return Math.sqrt(Math.pow(point2.x - point1.x, 2) + Math.pow(point2.y - point1.y, 2))
 }
 
+//wrapper function for text and affected keypooints
 function feedbackWrapper(text, keypoints) {
   return {text: text, keypoints: keypoints}
 }
 
+//angle between 3 points, calculate angle of the pivot point
 function angleBetween3Points(point1, pivot, point2) {
   const b = distanceBetween2Points(point1, pivot)
   const a = distanceBetween2Points(pivot, point2)
@@ -59,6 +44,7 @@ function angleBetween3Points(point1, pivot, point2) {
   return pivotAngleInDegrees
 }
 
+//checks wheter the keypoints are useable (not null and score > mpc)
 function useableKeypoints(kps, mpc) {
   for (let i = 0; i < kps.length; i++) {
     if (kps[i] === null || kps[i].score < mpc) {
@@ -68,50 +54,25 @@ function useableKeypoints(kps, mpc) {
   return true
 }
 
-/*function getOrientedAngle(angle, orientation) {
-  var changedAngle = angle
-
-  switch (orientation) {
-    case "N":
-      // do nothing (right orientation)
-      break;
-    case "E":
-      changedAngle = angle + 90
-      break;
-    case "S":
-      changedAngle = angle + 180
-      break;
-    case "W":
-      changedAngle = angle - 90
-      break;
-  }
-
-  while (changedAngle > 90) {
-    changedAngle -= 90
-  }
-
-  while (changedAngle < -90) {
-    changedAngle += 90
-  }
-
-  return changedAngle
-}*/
-
 //===============================================BUILDING BLOCKS=====================================================
 
+/*
+* General explenation of angles:
+* Angles that are given with the functions are in degrees
+* North is 90° ( we don't use South because we can calculate all South with North)
+* East & West are 0°
+* North-East is negative (-89 till 0)
+* North-West is positive (89 till 0)
+* */
+
 /**
- * Gives the distance between 2 lines, calculated from 4 points,
+ * Checks if distance between 2 lines, calculated from 4 points,
  * line1: point1 and point2
  * line2: point3 and point4
- * @param kps
- * @param point1
- * @param point2
- * @param point3
- * @param point4
- * @param distance
- * @param feedback
- * @param mpc
- * @return {*}
+ * are higher then given distance parameter
+ * @param kps keypoints
+ * @param mpc minimum part confidence
+ * @return {Object} with feedback text and the keypoints that it applies to
  */
 function distanceBetween2Lines(kps, point1, point2, point3, point4, distance, feedback, mpc) {
   const p1 = kps[point1]
@@ -139,14 +100,11 @@ function distanceBetween2Lines(kps, point1, point2, point3, point4, distance, fe
 }
 
 /**
- * See if the 2 given keypoints are horizontal.
- * @param kps
- * @param {*} point1: First key-point
- * @param {*} point2: Second key-point
- * @param {Number} mpc: The minPartConfidence
- * @param {Number} threshold: The error in degrees that is allowed
- * @param {String} feedback: The string that is returned when kepoints are not horizontal
- * @returns {Object} feedback The feedback that is returned when keypoints are not horizontal
+ * Check if the 2 given keypoints are horizontal.
+ * @param kps keypoints
+ * @param threshold gives us a margin in degrees, to combat the lack of reliability
+ * @param mpc minimum part confidence
+ * @return {Object} with feedback text and the keypoints that it applies to
  */
 function horizontal(kps, point1, point2, threshold, feedback, mpc) {
   const p1 = kps[point1]
@@ -162,17 +120,14 @@ function horizontal(kps, point1, point2, threshold, feedback, mpc) {
   return feedbackWrapper("", [])
 }
 
-//TODO: fix this one!
 /**
- *
- * @param kps
- * @param point1
- * @param point2
- * @param threshold
- * @param feedback1
- * @param feedback2
- * @param mpc
- * @return {string|*}
+ * Check if 2 given keypoints are vertical.
+ * @param kps keypoints
+ * @param threshold gives us a margin in degrees, to combat the lack of reliability
+ * @param feedback1 given feedback text when angle is to much to the right
+ * @param feedback2 given feedback text when angle is to much to the left
+ * @param mpc minimum part confidence
+ * @return {Object} with feedback text and the keypoints that it applies to
  */
 function vertical(kps, point1, point2, threshold, feedback1, feedback2, mpc) {
   const p1 = kps[point1]
@@ -182,7 +137,6 @@ function vertical(kps, point1, point2, threshold, feedback1, feedback2, mpc) {
     const angle = Math.atan((p2.position.y - p1.position.y) / (p2.position.x - p1.position.x)) * 180 / Math.PI;
 
     if (angle < 0 && angle > -90 + threshold) {
-      //too much to the right (need to move left)
       return feedbackWrapper(feedback1, [{id: point1, part: p1.part}, {id: point2, part: p2.part}]);
     } else if (angle > 0 && angle < 90 - threshold) {
       //too much to the left (need to move right)
@@ -193,16 +147,13 @@ function vertical(kps, point1, point2, threshold, feedback1, feedback2, mpc) {
 }
 
 /**
- *
- * @param kps
- * @param point1: highest point
- * @param point2: lowest point
- * @param angle1
- * @param angle2
- * @param feedback1
- * @param feedback2
- * @param mpc
- * @return {string|*}
+ * Check if 2 given keypoints are vertical between angle1 and angle2.
+ * @param kps keypoints
+ * @param angle1 angle2 these angles describe the allowed range
+ * @param feedback1 given feedback text when angle is to much to the right
+ * @param feedback2 given feedback text when angle is to much to the left
+ * @param mpc minimum part confidence
+ * @return {Object} with feedback text and the keypoints that it applies to
  */
 function verticalRange(kps, point1, point2, angle1, angle2, feedback1, feedback2, mpc) {
   const p1 = kps[point1]
@@ -246,16 +197,13 @@ function verticalRange(kps, point1, point2, angle1, angle2, feedback1, feedback2
 
 /**
  * Checks if angle formed between 2 lines, defined from 3 points is equal to given angle +- threshold
- * @param kps
- * @param point1
- * @param pivot
- * @param point2
- * @param angle
- * @param threshold
- * @param feedback1: feedback string returned when calculated angle is smaller then given angle
- * @param feedback2: feedback string returned when calculated angle is bigger then given angle
- * @param mpc
- * @return {Object} returns proper text feedback
+ * @param kps keypoints
+ * @param point1, pivot, point2 form a triangle. We calculate the angle of the pivot
+ * @param threshold gives us a margin in degrees, to combat the lack of reliability
+ * @param feedback1 given feedback text when angle is to much to the right
+ * @param feedback2 given feedback text when angle is to much to the left
+ * @param mpc minimum part confidence
+ * @return {Object} with feedback text and the keypoints that it applies to
  */
 function angle(kps, point1, pivot, point2, angle, threshold = 1,
                feedback1 = "Angle is too small.",
@@ -284,16 +232,13 @@ function angle(kps, point1, pivot, point2, angle, threshold = 1,
 
 /**
  * Checks if angle formed between 2 lines, defined from 3 points, needs to be between angle1 and angle2
- * @param kps
- * @param point1
- * @param pivot
- * @param point2
- * @param angle1
- * @param angle2
- * @param feedback1: feedback string returned when calculated angle is smaller then given angle
- * @param feedback2: feedback string returned when calculated angle is bigger then given angle
- * @param mpc
- * @return {string} returns proper text feedback
+ * @param kps keypoints
+ * @param point1, pivot, point2 form a triangle. We calculate the angle of the pivot
+ * @param angle1 angle2 these angles describe the allowed range
+ * @param feedback1 given feedback text when angle is to much to the right
+ * @param feedback2 given feedback text when angle is to much to the left
+ * @param mpc minimum part confidence
+ * @return {Object} with feedback text and the keypoints that it applies to
  */
 function angleRange(kps, point1, pivot, point2, angle1, angle2,
                     feedback1 = "Angle is too small.",
@@ -321,13 +266,18 @@ function angleRange(kps, point1, pivot, point2, angle1, angle2,
 
 //=============================================== MAIN ==============================================================
 
+/**
+ * Checks if person is fully in screen
+ * @param kps keypoints
+ * @param feedback given feedback text when person is not fully in screen
+ * @param mpc minimum part confidence
+ * @return {Object} with feedback text and the keypoints that it applies to
+ */
 function inScreen(kps, feedback, mpc) {
   let count = 0
   for (let i = 0; i< kps.length; i++) {
-    if (kps[i].score > mpc)
+    if (kps[i] === null || kps[i].score < mpc)
       count++
-    //else
-      // console.log(JSON.stringify(kps[i]))
   }
   if (count !== 17) {
     return feedbackWrapper(feedback, [])
@@ -340,7 +290,7 @@ function inScreen(kps, feedback, mpc) {
  * @param keypoints An array of key-points of the user
  * @param check
  * @param mpc minPartConfidence
- * @return {*}
+ * @return {Object} Check, contains the feedback with the affected keypoints
  */
 export function checkHeuristics(keypoints, check, mpc) {
   // get in screen (init)
@@ -413,22 +363,5 @@ function checkFrontSideSquat(keypoints, mpc) {
   return feedbackArray
 }
 
-//TODO: implement 3 exercises like the example above
-//1. horizontal shoulders
-//2. vertical posture (feet need to be as wide as shoulders)
-//3. angle of knee lies between 180 and 90 degrees
+//TODO: implement exercise (MARK)
 
-function excersise1() {
-  var array = []
-  return array
-}
-
-function excersise2() {
-  var array = []
-  return array
-}
-
-function excersise3() {
-  var array = []
-  return array
-}
